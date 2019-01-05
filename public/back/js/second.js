faef785fe2d001e67eaa694ac0efe3d8fbb27343
ExcellentFlower <Little_Xian.qq.com>
2019-01-05 22:04:47 +0800

    二级分类页面全部完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2019/1/5.
  */
-$(function(){
+$(function () {
     var currentPage = 1;
     var pageSize = 5;
     rander()
@@ -34,4 +34,52 @@ $(function(){
             }
         });
     }
+
+    $('#addCategory').on('click', function () {
+        $.ajax({
+            url: '/category/queryTopCategoryPaging',
+            type: 'get',
+            dataType: 'json',
+            data: {
+                page: 1,
+                pageSize: 1000
+            },
+            success: function (res) {
+                var str = template('categoryCon', res);
+                $('.dropdown-menu').html(str);
+            }
+        });
+    });
+    $('.dropdown-menu').on('click','li',function(){
+        //console.log($(this).find('a').text());
+        $('.dropdownText').text($(this).find('a').text());
+        $('.dropdownText').attr('data-id',$(this).attr('data-id'));
+    });
+    $('#pic').fileupload({
+        dataType:"json",
+        //e：事件对象
+        //data：图片上传后的对象，通过data.result.picAddr可以获取上传后的图片地址
+        done:function (e, data) {
+            console.log(data);
+            $('#imgBox img').attr('src',data.result.picAddr);
+        }
+    });
+    $('#btnAdd').on('click',function(){
+        $.ajax({
+            url: '/category/addSecondCategory',
+            type: 'post',
+            dataType: 'json',
+            data: {
+                brandName: $('.brandName').val(),
+                categoryId: $('.dropdownText').attr('data-id'),
+                brandLogo: $('#imgBox img').attr('src')
+            },
+            success: function (res) {
+                $('#myModal2').modal('hide')
+                rander();
+                //var str = template('categoryCon', res);
+                //$('.dropdown-menu').html(str);
+            }
+        });
+    });
 })
