0344da09188fd0f32026ac395ce5e8afcc5d5481
ExcellentFlower <Little_Xian.qq.com>
2019-01-05 09:32:45 +0800

    首页公共样式及js效果完成

--- public/back/js/comment.js
+++ public/back/js/comment.js
@@ -11,8 +11,28 @@ $(function(){
                 return;
             }
             else {
-                location.href = "user.html";
+                location.href = "login.html";
             }
         }
+    });
+    $('.category').click(function(){
+        $('.child').slideToggle();
+    });
+    $('.icon_menu').click(function(){
+        $('.aside').toggleClass('hid');
+        $('.main').toggleClass('hid');
+        $('.topbar').toggleClass('hid');
+    });
+    $('#loginOut').click(function(){
+        $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            dataType:'json',
+            success:function(res){
+                if(res.success){
+                    location.href = "login.html";
+                }
+            }
+        });
     });
 })
